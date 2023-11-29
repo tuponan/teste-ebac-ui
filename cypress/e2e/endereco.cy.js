0e43485831f5ab66c9aea5d83e9ff0f3fbffd020
Tuponan mello <grandetupa@gmail.com>
2023-11-29 14:41:55 -0300

    usando arquivo de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,10 @@
 /// <reference types="cypress" />
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
+
+
+enderecoPage
+
 
 describe('Funcionalidade endereços - Faturamento e Entrega', () => {
   beforeEach(() => {
@@ -10,6 +16,27 @@ describe('Funcionalidade endereços - Faturamento e Entrega', () => {
   });
 
   it('Deve fazer cadastro de faturamento com sucesso', () => {
+    enderecoPage.editarEnderecoFaturamento('Cleberson', 'Jhonsons', 'EBACSHOP', 'Brasil', 'AV dos coqueiros', '546', 'Campinas', 'São Paulo', '01000100', '40028922', 'email@email.com')
 
+    cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
   });
+
+  it('Deve fazer cadastro de faturamento com sucesso - usando arquivo de dados', () => {
+    enderecoPage.editarEnderecoFaturamento(
+      dadosEndereco[2].nome,
+      dadosEndereco[2].sobrenome,
+      dadosEndereco[2].empresa,
+      dadosEndereco[2].pais,
+      dadosEndereco[2].endereco,
+      dadosEndereco[2].numero,
+      dadosEndereco[2].cidade,
+      dadosEndereco[2].estado,
+      dadosEndereco[2].cep,
+      dadosEndereco[2].telefone,
+      dadosEndereco[2].email
+      )
+
+    cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')
+  });
+
 });
